731e8f1afe1f3f010e734469600ce9551430f193
Adinath more <moreadi7777@gmail.com>
2025-08-12 21:23:03 +0530

    nested loop ex

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-for (var a =1; a <= 100; a = a + 10){
-    for (var b = a; b < a + 10; b++){
+for (var a = 1; a<= 5; a++){
+    for (var b = 1; b <= a; b++){
         document.write(b + " ");
-        }
-        document.write("<br>");
+    }
+    document.write("<br>");
 }
