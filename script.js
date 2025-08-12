d6c88a42d0a311f904b4a20c01dd0998a0588be1
Adinath more <moreadi7777@gmail.com>
2025-08-12 08:37:47 +0530

    break statement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 for (var a =1; a <= 10; a++){
     if (a == 3){
         document.write("Hey :" + a + "<br>");
-        continue;      //this is a imp
+        break;      //this is a imp
     }
     document.write("Number :" + a + "<br>");
 }
