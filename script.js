8aac8287a5031c9099d2720ee5d6a52bd0fa0c25
Adinath more <moreadi7777@gmail.com>
2025-08-12 08:35:47 +0530

    continue statement

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-for(var a =1; a <= 10; a++){
-    document.write(a+ ") WellCome To My Code" + "<br>");
+for (var a =1; a <= 10; a++){
+    if (a == 3){
+        document.write("Hey :" + a + "<br>");
+        continue;      //this is a imp
+    }
+    document.write("Number :" + a + "<br>");
 }
