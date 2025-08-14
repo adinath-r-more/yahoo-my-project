132406b342df7ad36526590040941de9f3786a14
Adinath more <moreadi7777@gmail.com>
2025-08-14 21:16:08 +0530

    Join Array

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var a = ["Sanjay", "Aman", "Rohit"];
 var b = ["Rahul", "Yogesh"];
-var d = ["Neha", "Mahima"];
 
-var c = a.concat(b,d);
+var c =a.concat (b);
 
-document.write(c);
+document.write(c + "<br><br>");
+
+var d = c.join(" / ");  //yaha pr space ya kuch bhi add kar sakate hai
+
+document.write(d);  
