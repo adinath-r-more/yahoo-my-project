093d3fcf93cf7e11a84015009bc1fbe5831591f2
Adinath more <moreadi7777@gmail.com>
2025-08-20 19:02:32 +0530

    Find Array

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var ages =[100,130,180,250];
 document.writeln(ages + "<br><br>");
 
-var b = ages.every (checkAdult);
+var b = ages.find (checkAdult);
     document.writeln(b+ "<br><br>");
 
     function checkAdult (age){
-        return age >= 50;
+        return age >= 170;
     }
         
   
