33f7aeee268f838456cde3455824955a1d494451
Adinath more <moreadi7777@gmail.com>
2025-08-20 18:42:11 +0530

    includes Array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var a = ["Sanjay", "Aman", "Ravi", "Neha", "Aman"];
 document.write(a+ "<br><br>");
 
-var c = a.lastIndexOf ( "Ravi", 2);
-document.write(c+ "<br>");
+var b = a.includes ( "Aman");
+document.write(b+ "<br>");
