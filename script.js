01d7fbb2048fccaf78ea0bb521975b83e9ebec74
Adinath more <moreadi7777@gmail.com>
2025-08-21 11:14:56 +0530

    toString Array

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var ages =[10,23,19,30];
-document.writeln(ages + "<br><br>");
-
-var b = ages.filter (checkAdult);
-    document.writeln(b+ "<br><br>");
-
-    function checkAdult (age){
-        return age >= 20;
-    }
-        
-  
+var a =["Rahul", "Karan", "Neha", "Aman"];
+a.toString();
+document.writeln(a);
