41d8129d8c3d2bd2a193ad1027c3b14cd672627d
Adinath more <moreadi7777@gmail.com>
2025-08-27 14:02:21 +0530

    Array Of Obj

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var person = new Object();
-
-person.firstName = 'Rohit';
-person.lastName = 'Jadhav';
-person.age = 25;
-
-console.log(person);
+var student =[
+    {name :'Ram', age : 25},
+    {name :'Anu', age : 15},
+    {name :'OM', age : 19}
+];
+for (var a = 0; a < student.length; a++ ){
+    document.writeln(student[a].name + " : " + student[a].age + "<br>");
+}
