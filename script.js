237175de1015e44ae2864fd7af2dd6b367ab7fef
Adinath more <moreadi7777@gmail.com>
2025-08-27 14:25:38 +0530

    For / In Loop

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const a ={
-    name : "Ramesh",
-    age : 45
-};
-console.log(a);
+var obj = {
+    firstName : "Adi",
+    lastName : "More",
+    age : 25,
+    email : "hello@123.net"
+}
 
-a.name = "Avi";
-a.age = 55;
-
-console.log(a);
+for (var key in obj){
+    document.writeln(key + " : "+ obj[key]+ "<br>");
+}
