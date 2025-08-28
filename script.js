89ce92887642c311df3298250aecf9fb98b28041
Adinath more <moreadi7777@gmail.com>
2025-08-28 20:33:32 +0530

    Map Method

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var obj = {
-    firstName : "Adi",
-    lastName : "More",
-    age : 25,
-    email : "hello@123.net"
-}
+var ary = [11,5,9,20];
 
-for (var key in obj){
-    document.writeln(key + " : "+ obj[key]+ "<br>");
+document.writeln(ary + "<br><br>");
+
+var b = ary.map (Text);
+document.writeln(b);
+
+function Text (x){
+    return x * 10;
 }
