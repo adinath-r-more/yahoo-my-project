11b897942e998ae03bff96f9ad8e399627683de3
Adinath more <moreadi7777@gmail.com>
2025-09-16 20:50:30 +0530

    querySelectorAll innerHTML

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var element;
 
 document.querySelector("#header").innerHTML = "<h1>WoW</h1>";
 
-element=document.querySelectorAll(".list")[1].innerHTML;
+element=document.querySelectorAll("ul")[1].innerHTML;
 
 console.log(element);
