542bd903605f27dae0c21ef442992947528ba313
Adinath more <moreadi7777@gmail.com>
2025-09-16 20:31:11 +0530

    removeAttribute

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById("header").innerHTML = "<h1>WoW</h1>";
 
 document.getElementById("header").attributes [1].value;
 
+document.getElementById("header").removeAttribute("style");
+
 element=document.getElementById("header").getAttribute("class") ;
 
 
