494a156d7de227a861f007427ca72b2188ee4811
Adinath more <moreadi7777@gmail.com>
2025-09-16 20:09:25 +0530

    Dom Set Method

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var element;
-element=document.getElementById("header").attributes [2] ;
+document.getElementById("header").innerHTML = "<h1>WoW</h1>";
+element=document.getElementById("header").innerHTML ;
 
 console.log(element);
