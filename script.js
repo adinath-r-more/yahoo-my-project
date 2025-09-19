e1df2e597995d19f2f31118b2ba333314a4ee785
Adinath more <moreadi7777@gmail.com>
2025-09-19 22:16:23 +0530

    addEventListener

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ document.getElementById("header").addEventListener("click",abc);
 
 function abc(){
 document.getElementById("header").classList.add("xxy");
+
+var a = document.getElementById("header").classList;
+console.log(a);
 }
