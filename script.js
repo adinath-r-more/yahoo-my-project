00333c8d8f116d1eaf4d176799fc68cd40014595
Adinath more <moreadi7777@gmail.com>
2025-09-21 20:53:54 +0530

    firstChild

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById("inner").lastElementChild.style.background = "red";
+// document.getElementById("inner").firstChild.style.background = "red";
 
-var a = document.getElementById("inner").lastElementChild;
+var a = document.getElementById("inner").firstChild;
 
 console.log(a);
