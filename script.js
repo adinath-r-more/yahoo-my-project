6577f91e94258109e1d6f9d831c5855146ab568b
Adinath more <moreadi7777@gmail.com>
2025-09-22 22:46:02 +0530

    createElement

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-var newElement = document.createElement("p");
+var newElement = document.createElement("h4");
+
+var newText = document.createTextNode("This is just Text");
 
 console.log(newElement);
+console.log(newText);
