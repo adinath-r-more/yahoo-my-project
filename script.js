33e04ca6f0d187d3078d6ab084564af65dc10988
Adinath more <moreadi7777@gmail.com>
2025-09-23 20:14:15 +0530

    appendChild

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// var newElement = document.createElement("h4");
+var newElement = document.createElement("h4");
 
-// var newText = document.createTextNode("This is just Text");
+var newText = document.createTextNode("This is just Text");
 
-var newComment = document.createComment("This Is Comment");
+newElement.appendChild(newText);
 
-// console.log(newElement);
-// console.log(newText);
-console.log(newComment );
+console.log(newElement);
+
+console.log(newText);
