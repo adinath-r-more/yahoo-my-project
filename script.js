34bc59a487e36257ff66c0c41ce3ca8d85078276
Adinath more <moreadi7777@gmail.com>
2025-09-23 20:30:40 +0530

    insertBefore

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var newElement = document.createElement("h4");
 
 var newText = document.createTextNode("This is just Text");
 
-var newComment = document.createComment("This is just Comment");
-
 newElement.appendChild(newText);
 
-document.getElementById("text").appendChild(newElement);
-document.getElementById("text").appendChild(newComment);
+// document.getElementById("text").appendChild(newElement);
 
-console.log(newElement); 
+var target = document.getElementById("text");
+
+target.insertBefore(newElement,target.childNodes[0]);
