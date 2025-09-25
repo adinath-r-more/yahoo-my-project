363eee09166e6cab9017ad0b2988966da5c2d5ef
Adinath more <moreadi7777@gmail.com>
2025-09-25 22:13:43 +0530

    clearInterval

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var a = 0;
-setInterval(Anim, 500);
+var id = setInterval(Anim, 50);
 
 function Anim(){
     a = a + 10;
+    if(a == 1500){
+        clearInterval(id);
 
- var target = document.getElementById("text");
- target.style.marginLeft = a + 'px';
+    }else{
+     var target = document.getElementById("text");
+        target.style.width = a + 'px';
+    }
+
+
 }
