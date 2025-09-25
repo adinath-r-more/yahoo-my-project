f3bedccdc9aaac8e26e5576243fc783d8d8bf676
Adinath more <moreadi7777@gmail.com>
2025-09-25 23:01:01 +0530

    clearTimeout inbuild Function

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var id = setTimeout(abc , 6000);
-
-function abc(){
-    var target = document.getElementById("text");
+var id = setTimeout(function(){
+     var target = document.getElementById("text");
     target.style.width = "500px";
-}
+} , 6000);
+
 
 function StopAnimation(){
     clearTimeout(id);
