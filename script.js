e850c772ab6760e97496ff6ec3253684921a4146
Adinath more <moreadi7777@gmail.com>
2025-09-26 21:32:40 +0530

    onresize

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var iHeight = window.innerHeight;
+
+function resizeFunction(){
+    console.clear();
+    var iHeight = window.innerHeight;
 console.log('inner Height : ' + iHeight);
 
 var oHeight = window.outerHeight;
 console.log( 'outer Height : ' + oHeight); 
+
+}
