a35ecf6a87207061e91a27a798f2905a559947e0
Adinath more <moreadi7777@gmail.com>
2025-09-26 21:38:29 +0530

    outerWidth

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function resizeFunction(){
     var iWidth = window.innerWidth;
 console.log('inner Width : ' + iWidth);
 
-// var oWidth = window.outerWidth;
-// console.log( 'outer Width : ' + oWidth); 
+var oWidth = window.outerWidth;
+console.log( 'outer Width : ' + oWidth); 
  
 }
