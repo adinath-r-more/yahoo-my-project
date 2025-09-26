9e3df9c640d8bed439ddee17973358fb7f79bb92
Adinath more <moreadi7777@gmail.com>
2025-09-26 21:43:21 +0530

    openWindow

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-function resizeFunction(){
-    console.clear();
-    var iWidth = window.innerWidth;
-console.log('inner Width : ' + iWidth);
-
-var oWidth = window.outerWidth;
-console.log( 'outer Width : ' + oWidth); 
- 
+function openWindow(){
+    window.open("http://www.yahoobaba.net");
 }
