7283249e02583497073bd6bef7f9683b22f5761e
Adinath more <moreadi7777@gmail.com>
2025-09-27 22:53:39 +0530

    moveBy

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var mywindow;
 function openWindow(){
-    mywindow =  window.open("width=500px,height=200px");
+    mywindow =  window.open("width=500px,height=200px, left=100px, top=100px");
     mywindow.document.write("<p>This is My Window</p>");
 }
 function moveWindow(){
-   mywindow.moveTo(100,100);
+   mywindow.moveBy(500,500);
    mywindow.focus();
 }
 
