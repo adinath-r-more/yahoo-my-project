86057034fdcdc5e91b2ccbf447341eeb7c6e7a8d
Adinath more <moreadi7777@gmail.com>
2025-09-27 23:14:29 +0530

    scrollBy

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var myWindow;
-function openWindow(){
-    myWindow =  window.open("width=200px,height=200px, left=30px, top=30px");
-    myWindow.document.write("<p>This is Mywindow.</p>");
+function scrollWindow(){
+    window.scrollBy(0,20);
 }
-function resizeWindow(){
-   myWindow.resizeBy(100, 100);
-}
-
