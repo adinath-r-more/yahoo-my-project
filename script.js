e232424dfc78975a5db218849b3d6965029dfca0
Adinath more <moreadi7777@gmail.com>
2025-09-29 20:16:09 +0530

    scrollLeft

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const target = document.querySelector("#Box");
 
 target.addEventListener('scroll',() => {
     console.clear();
+    console.log("scrollLeft : " + target.scrollLeft);
     console.log("scrollTop : " + target.scrollTop);
     
 });
