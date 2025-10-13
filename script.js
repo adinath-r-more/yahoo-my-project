41c27a401e594ff5c017a0e4b39b5e92a0919a27
Adinath more <moreadi7777@gmail.com>
2025-10-13 19:03:46 +0530

    abc.y

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var target = document.querySelector("#Box");
 
 target.addEventListener('mousemove', function (abc) {
     console.clear();
-    var x = abc.x;
+    // var x = abc.x;
     var y = abc.y;
 
-    console.log("Client :" + x);
+    // console.log("Client :" + x);
     console.log("Client :" + y);
 })
