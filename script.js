8bbb4f24c20748a8c168e1b87eea239288f0b3b4
Adinath more <moreadi7777@gmail.com>
2025-10-13 18:38:57 +0530

    scrollHeight

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const target = document.querySelector("#Box");
 
-var scroll_width = target.scrollWidth;
-// var scroll_height = target.scrollHeight;
-    console.log("scrollWidth : " + target.scrollWidth);
-    // console.log("scrollHeight : " + target.scrollHeight);
+// var scroll_width = target.scrollWidth;
+var scroll_height = target.scrollHeight;
+    // console.log("scrollWidth : " + target.scrollWidth);
+    console.log("scrollHeight : " + target.scrollHeight);
     
