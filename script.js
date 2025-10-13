5340d9cc2be4582d8d8c4841489aefd02b2ae7b1
Adinath more <moreadi7777@gmail.com>
2025-10-13 18:56:06 +0530

    clientHeight

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var target = document.querySelector("#Box");
 
-var Width = target.clientWidth;
-console.log("clientHeight :" + Width);
+var Height = target.clientHeight;
+console.log("clientHeight :" + Height);
 
