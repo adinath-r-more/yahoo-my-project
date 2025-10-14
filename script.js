89811856be310387ef6cc2841e25eb48f835fa8a
Adinath more <moreadi7777@gmail.com>
2025-10-14 21:15:46 +0530

    ScreenX

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 document.querySelector('#box').addEventListener('mousemove', function (abc) {
     console.clear();
     var x = abc.ScreenX;
-    var y = abc.ScreenY;
+    // var y = abc.ScreenY;
 
     console.log("Screen x :" + x);
-    console.log("Screen y :" + y);
+    // console.log("Screen y :" + y);
 });
