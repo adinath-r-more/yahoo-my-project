3210bf81783d67220fce46cf10353bfef1938b0e
Adinath more <moreadi7777@gmail.com>
2025-10-14 21:00:55 +0530

    pageY

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 document.addEventListener('mousemove', function (abc) {
     console.clear();
-    var x = abc.pageX;
-    // var y = abc.pageY;
+    // var x = abc.pageX;
+    var y = abc.pageY;
 
-    console.log("Page x :" + x);
-    // console.log("Page y :" + y);
+    // console.log("Page x :" + x);
+    console.log("Page y :" + y);
 });
