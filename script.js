8c44e1413076b6fa6386f9c2e6b3209146d61c7b
Adinath more <moreadi7777@gmail.com>
2025-10-15 22:20:01 +0530

    altKey

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var target = document.querySelector('#box');
 target.addEventListener('click', function(e){
     console.clear();
 
-    var k = e.shiftKey;
+    var k = e.altKey;
 
     console.log(k);
 });
