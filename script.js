3ed29509c920eca71f12b7171f90ced487604a7b
Adinath more <moreadi7777@gmail.com>
2025-10-15 22:16:48 +0530

    ctrlKey

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var target = document.querySelector('#box');
 
-target.addEventListener('mouseover', function(e){
+target.addEventListener('click', function(e){
     console.clear();
-    // var x = e.offsetX;
-    var y = e.offsetY;
 
-    // console.log('offset X :' + x);
-    console.log('offset Y :' + y);
+    var k = e.ctrlKey;
+
+    console.log(k);
 });
