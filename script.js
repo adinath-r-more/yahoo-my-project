d12dfd8538b37a5407432c0f7655b3870040a43a
Adinath more <moreadi7777@gmail.com>
2025-10-15 22:11:49 +0530

    offsetY

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var target = document.querySelector('#box');
 
 target.addEventListener('mouseover', function(e){
     console.clear();
-    var x = e.offsetX;
+    // var x = e.offsetX;
     var y = e.offsetY;
 
-    console.log('offset X :' + x);
+    // console.log('offset X :' + x);
     console.log('offset Y :' + y);
 });
