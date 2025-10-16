8cbe960833b6be1c4b72928570b60abef586bf5e
Adinath more <moreadi7777@gmail.com>
2025-10-16 22:01:41 +0530

    metaKey

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ var target = document.querySelector('#box');
 target.addEventListener('click', function(e){
     console.clear();
 
-    var k = e.metaKey;
+    var k;
+    switch (true){
+        case e.metaKey:
+            k= "Meta Key";
+            break;
+
+            // case e.altKey:
+            // k= "alt Key";
+            // break;
+
+            // case e.ctrlKey:
+            // k= "ctrl Key";
+            // break;
+
+            // case e.shiftKey:
+            // k= "Shift Key";
+            // break;
+
+            default:
+                k = "No Key Pressed";
+                break;
+    }
 
     console.log(k);
+
+    
 });
