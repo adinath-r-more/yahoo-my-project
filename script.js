bb6c9ff23a13d7cccaab83a5f63133e9903724f6
Adinath more <moreadi7777@gmail.com>
2025-10-16 22:25:49 +0530

    e.button background

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ var target = document.querySelector('#box');
 target.addEventListener('mousedown', function(e){
     console.clear();
 
-    var text = "You Passed Button :" + e.button;
-    console.log(text);
+    var text = e.button;
+    var color;
+    switch (text){
+        case 0:
+        color = "green";
+        break;
+
+        case 1:
+        color = "orange";
+        break;
+
+        case 2:
+        color = "blue";
+        break;
+
+        default:
+        color = "pink";
+        break;
+    }
+    target.style.background = color;
+    // console.log(text);
   
 });
