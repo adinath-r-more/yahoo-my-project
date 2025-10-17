1b6b79706beffb45f5a11d43af68ec17e70cea9d
Adinath more <moreadi7777@gmail.com>
2025-10-17 17:02:35 +0530

    offline function for body tag

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var target = document.querySelector("#box");
 
-window.addEventListener('offline', function(){
-    target.innerHTML = "Your Offline.";
-    target.style.background = "pink";
-});
+// window.addEventListener('offline', function(){
+//     target.innerHTML = "Your Offline.";
+//     target.style.background = "pink";
+// });
 
 window.addEventListener('online', function(){
     target.innerHTML = "Your Online.";
     target.style.background = "lightblue";
 });
+
+function off(){
+     target.innerHTML = "Your Offline.";
+    target.style.background = "pink";
+}
