e366192f407b54e6c9da4f06e9746699fb29784e
Adinath more <moreadi7777@gmail.com>
2025-10-17 16:21:14 +0530

    box Copy style background color

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
-
 var target = document.querySelector('#input');
+var box = document.querySelector('#box');
 
 target.addEventListener('copy', function(){
     console.clear();
     console.log("You Copy Text.");
+    target.style.background = "pink";
 });
 
-// target.addEventListener('cut', function(){
-//     console.clear();
-//     console.log("You Cut Text.");
-// });
+target.addEventListener('cut', function(){
+    console.clear();
+    console.log("You Cut Text.");
+    target.style.background = "orange";
+});
 
-// target.addEventListener('paste', function(){
-//     console.clear();
-//     console.log("You Pasted Text.");
-// });
+target.addEventListener('paste', function(){
+    console.clear();
+    console.log("You Pasted Text.");
+    target.style.background = "lightblue";
+});
+
+box.addEventListener('copy', function(){
+    box.style.background = "orange"
+    });
