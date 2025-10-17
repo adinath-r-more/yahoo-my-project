1d821ba29cd76c7bb338203283f5ea2a5033bd31
Adinath more <moreadi7777@gmail.com>
2025-10-17 15:48:39 +0530

    cut

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 var target = document.querySelector('#input');
 
-target.addEventListener('copy', function(){
-    console.clear();
-    console.log("You Copy Text.");
-});
+// target.addEventListener('copy', function(){
+//     console.clear();
+//     console.log("You Copy Text.");
+// });
 
 target.addEventListener('cut', function(){
     console.clear();
     console.log("You Cut Text.");
 });
 
-target.addEventListener('paste', function(){
-    console.clear();
-    console.log("You Pasted Text.");
-});
+// target.addEventListener('paste', function(){
+//     console.clear();
+//     console.log("You Pasted Text.");
+// });
